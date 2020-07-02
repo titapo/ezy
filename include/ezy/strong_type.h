e8a30192013815577914e190d4e95bee0ba437ec
Abel Suranyi <suranyiabel@gmail.com>
2020-07-02 22:49:37 +0200

    split strong type
    It was expected to reduce template instantiation costs, but there is not
    noticeable difference in compilation time (as far as tests concerned)

--- include/ezy/strong_type.h
+++ include/ezy/strong_type.h
@@ -56,56 +56,83 @@ namespace ezy
   using notag_t = void;
   using extended_tag_t = void;
 
-  template <typename T, typename Tag, template<typename> class... Features>
-  class strong_type : public Features<strong_type<T, Tag, Features...>>...
+  namespace detail
   {
-    private:
-      // introduced as a dependent typedef, to defer evaluation
-      template <typename...>
-      using is_extended = std::is_same<Tag, extended_tag_t>;
+    template <typename Tag, typename...>
+    using is_tag_extended = std::is_same<Tag, extended_tag_t>;
 
+    template <typename T, bool Extended = false>
+    struct strong_type_payload
+    {
+      strong_type_payload() = default;
+
+      strong_type_payload(const strong_type_payload&) = default;
+      strong_type_payload& operator=(const strong_type_payload&) = default;
+
+      strong_type_payload(strong_type_payload&&) = default;
+      strong_type_payload& operator=(strong_type_payload&&) = default;
+
+      template <typename Arg0, typename... Args>
+      constexpr explicit strong_type_payload(Arg0&& arg0, Args&&... args
+          //, std::enable_if_t<std::is_constructible_v<type, Args...>>* = nullptr
+          //, std::enable_if_t<detail::is_braces_constructible<T, Args...>::value>* = nullptr
+          //, std::enable_if_t<(sizeof...(Args) != 1) || (!std::is_same_v<ezy::remove_cvref_t<typename detail::headof<Args...>::type>, strong_type_payload>)>* = nullptr
+          )
+        : _value{std::forward<Arg0>(arg0), std::forward<Args>(args)...}
+      {}
+
+      T _value{};
+    };
+
+    template <typename T>
+    struct strong_type_payload<T, true/*extended*/>
+    {
+      strong_type_payload() = default;
+
+      strong_type_payload(const strong_type_payload&) = default;
+      strong_type_payload& operator=(const strong_type_payload&) = default;
+
+      strong_type_payload(strong_type_payload&&) = default;
+      strong_type_payload& operator=(strong_type_payload&&) = default;
+
+      template <typename Arg0, typename... Args>
+      constexpr strong_type_payload(Arg0&& arg0, Args&&... args)
+        : _value{std::forward<Arg0>(arg0), std::forward<Args>(args)...}
+      {}
+
+      T _value{};
+    };
+
+    template <typename T, bool IsExtended>
+    struct strong_type_base : detail::strong_type_payload<T, IsExtended>
+    {
+      using _payload = detail::strong_type_payload<T, IsExtended>;
+      using _payload::strong_type_payload;
+
+      constexpr T& get() & { return this->_value; }
+      constexpr decltype(auto) get() &&
+      {
+        if constexpr (std::is_lvalue_reference_v<T>)
+          return this->_value;
+        else
+          return std::move(this->_value);
+      }
+      constexpr const T& get() const & { return this->_value; }
+
+      explicit operator T() { return this->_value; }
+      explicit operator T() const { return this->_value; }
+    };
+  }
+
+  template <typename T, typename Tag, template<typename> class... Features>
+  class strong_type : public detail::strong_type_base<T, detail::is_tag_extended<Tag>::value>, public Features<strong_type<T, Tag, Features...>>...
+  {
     public:
       using type = T;
       using self_type = strong_type;
 
-      strong_type() = default;
-
-      strong_type(const strong_type&) = default;
-      strong_type& operator=(const strong_type&) = default;
-
-      strong_type(strong_type&&) = default;
-      strong_type& operator=(strong_type&&) = default;
-
-      template <typename... Args, std::enable_if_t<std::conjunction_v<detail::constant<(sizeof...(Args) > 0)>, std::negation<is_extended<Args...>>>, bool> = true>
-      constexpr explicit strong_type(Args&&... args
-          //, std::enable_if_t<std::is_constructible_v<type, Args...>>* = nullptr
-          //, std::enable_if_t<detail::is_braces_constructible<T, Args...>::value>* = nullptr
-          //, std::enable_if_t<(sizeof...(Args) != 1) || (!std::is_same_v<ezy::remove_cvref_t<typename detail::headof<Args...>::type>, strong_type>)>* = nullptr
-          )
-        : _value{std::forward<Args>(args)...}
-      {}
-
-      template <typename... Args, std::enable_if_t<std::conjunction_v<detail::constant<(sizeof...(Args) > 0)>, is_extended<Args...>>, bool> = true>
-      constexpr strong_type(Args&&... args)
-        : _value{std::forward<Args>(args)...}
-      {}
-
-      constexpr T& get() & { return _value; }
-      constexpr decltype(auto) get() &&
-      {
-        if constexpr (std::is_lvalue_reference_v<T>)
-          return _value;
-        else
-          return std::move(_value);
-      }
-      constexpr const T& get() const & { return _value; }
-
-      explicit operator T() { return _value; }
-      explicit operator T() const { return _value; }
-
-      // swap
-    private:
-      T _value{};
+      using _base = detail::strong_type_base<T, detail::is_tag_extended<Tag>::value>;
+      using _base::strong_type_base;
   };
 
   // TODO if not already a reference?
