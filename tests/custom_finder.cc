be5f41ec713b9a89e86b2a3e070a821d5d299966
Abel Suranyi <suranyiabel@gmail.com>
2020-10-15 21:23:33 +0200

    added algo_custom_find feature

--- tests/custom_finder.cc
+++ tests/custom_finder.cc
@@ -52,17 +52,18 @@ SCENARIO("custom find")
 
 #include <ezy/experimental/function.h>
 
+const auto greater_than = ezy::experimental::curry(ezy::experimental::flip(std::greater<>{}));
+
 SCENARIO("custom find if")
 {
   static constexpr auto myfind_if = ezy::custom_find_if<fake_result_maker>{};
   GIVEN("a range of numbers")
   {
     std::vector v{1,2,3,4,5};
-    const auto gt = ezy::experimental::curry(ezy::experimental::flip(std::greater<>{}));
 
     WHEN("find for an element")
     {
-      auto found = myfind_if(v, gt(2));
+      auto found = myfind_if(v, greater_than(2));
       THEN("it returns an engaged, containing 3")
       {
         REQUIRE(found.engaged);
@@ -72,12 +73,63 @@ SCENARIO("custom find if")
 
     WHEN("find for a non-presenting element")
     {
-      auto found = myfind_if(v, gt(5));
+      auto found = myfind_if(v, greater_than(5));
       THEN("it returns an not engaded")
       {
         REQUIRE(!found.engaged);
       }
     }
   }
+}
 
+#include <ezy/features/algo_custom_find.h>
+
+template <typename T>
+using my_find_feature = ezy::features::algo_custom_find<fake_result_maker>::impl<T>;
+
+#include <ezy/strong_type.h>
+
+SCENARIO("custom find feature")
+{
+  using Strong = ezy::strong_type<std::vector<int>, struct Tag, my_find_feature>;
+
+  Strong st{1,2,3,4};
+
+  WHEN("find an exising element")
+  {
+    const auto found = st.find(3);
+    THEN("returns a fake_optional")
+    {
+      REQUIRE(found.engaged);
+      REQUIRE(found.value == 3);
+    }
+  }
+
+  WHEN("find an non-exising element")
+  {
+    const auto found = st.find(6);
+    THEN("returns a not-engaged fake_optional")
+    {
+      REQUIRE(!found.engaged);
+    }
+  }
+
+  WHEN("find_if an exising element")
+  {
+    const auto found = st.find_if(greater_than(3));
+    THEN("returns a fake_optional")
+    {
+      REQUIRE(found.engaged);
+      REQUIRE(found.value == 4);
+    }
+  }
+
+  WHEN("find_if an non-exising element")
+  {
+    const auto found = st.find_if(greater_than(4));
+    THEN("returns a not-engaged fake_optional")
+    {
+      REQUIRE(!found.engaged);
+    }
+  }
 }
