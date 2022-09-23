515d1f4b9cf2f796cae730316dac4bb942142153
Abel Suranyi <suranyiabel@gmail.com>
2022-09-23 21:15:05 +0200

    Revert "ezy::detail::dependent_forward extracted for internal reuse"
    This reverts commit e696969d36f2b7ba8c6a6fb269645630826e6d13.

--- include/ezy/bits/find.h
+++ include/ezy/bits/find.h
@@ -5,12 +5,31 @@
 
 #include <ezy/optional.h>
 #include <ezy/pointer.h>
-#include <ezy/bits/dependent_forward.h>
 
 namespace ezy
 {
   namespace detail
   {
+    template <typename T>
+    decltype(auto) dependent_forward_impl(T&& t, ezy::experimental::owner_category_tag, std::false_type)
+    { return std::move(t); }
+
+    template <typename T>
+    decltype(auto) dependent_forward_impl(T&& t, ezy::experimental::reference_category_tag, std::false_type)
+    { return &t; }
+
+    // TODO const cases are missing
+
+    template <typename Dependence, typename T>
+    decltype(auto) dependent_forward(T&& t)
+    {
+      return dependent_forward_impl(
+          std::forward<T>(t),
+          ezy::experimental::detail::ownership_category_t<Dependence>{},
+          std::is_const<Dependence>{}
+      );
+    }
+
     template <typename Range, typename Iterator>
     auto make_find_result(Iterator it, Iterator last)
     {
